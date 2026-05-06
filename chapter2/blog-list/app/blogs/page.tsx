fc832fece2ab29a1a4647ee8e77871d395994fb6
Theoxenos <theoxenos@pm.me>
2026-05-06 14:49:51 +0200

    Finish exercise 2.6

--- chapter2/blog-list/app/blogs/page.tsx
+++ chapter2/blog-list/app/blogs/page.tsx
@@ -1,22 +1,35 @@
 import { IBlog, getBlogs } from "@/app/services/blogService";
 import Link from "next/link";
 
-const Blogs = () => {
-  const blogs: IBlog[] = getBlogs().toSorted((a, b) => b.likes - a.likes);
+const Blogs = async ({searchParams} : {searchParams: Promise<{filter?: string}>}) => {
+  const blogs: IBlog[] = getBlogs();
 
+  const filter = await searchParams;
+  const filteredBlogs = blogs
+      .filter((blog) => blog.title.toLowerCase().includes(filter.filter?.toLowerCase() || ""))
+      .toSorted((a, b) => b.likes - a.likes);
+  
   return (
     <div>
       <h2>Blogs</h2>
-        {blogs.map((blog: IBlog) => (
+      <div style={{ marginBottom: "1.5rem" }}>
+        <form method="get">
+          <input type="text" name="filter" placeholder="Filter by title" defaultValue={filter.filter} />
+          <button type="submit">Filter</button>
+        </form>
+      </div>
+      <div>
+        {filteredBlogs.map((blog: IBlog) => (
             <div key={blog.id} style={{ border: "1px solid black", marginBottom: "0.5rem", padding: "0.5rem" }}>
-                <h2><Link href={`blogs/${blog.id}`}>{blog.title}</Link></h2>
-                <ul>
-                    <li>Author: {blog.author}</li>
-                    <li>Url: {blog.url}</li>
-                    <li>Likes: {blog.likes}</li>
-                </ul>
+              <h3><Link href={`blogs/${blog.id}`}>{blog.title}</Link></h3>
+              <ul>
+                <li>Author: {blog.author}</li>
+                <li>Url: {blog.url}</li>
+                <li>Likes: {blog.likes}</li>
+              </ul>
             </div>
         ))}
+      </div>
     </div>
   );
 };
