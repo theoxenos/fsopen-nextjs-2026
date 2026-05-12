5539ef7263efb0495d023012b64ca634729fd406
Theoxenos <theoxenos@pm.me>
2026-05-12 11:38:32 +0200

    Finish exercise 4.15

--- chapter2/blog-list/app/actions/register.ts
+++ chapter2/blog-list/app/actions/register.ts
@@ -4,11 +4,35 @@ import {users} from "@/app/db/schema";
 import bcrypt from "bcryptjs";
 import {db} from "@/app/db";
 import {redirect} from "next/navigation";
+import {getUserByUsername} from "@/app/services/userService";
 
-export const registerUser = async (formData: FormData) => {
+export const registerUser = async (prevState: {errors: string[]}, formData: FormData) => {
     const name = formData.get("name") as string;
-    const password = formData.get("password") as string;
     const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+    const confirmPassword = formData.get("confirmPassword") as string;
+
+    const errors = []
+    
+    if(username.length < 4) {
+        errors.push("Username must be at least 4 characters");
+    }
+    
+    if(password.length < 4) {
+        errors.push("Password must be at least 4 characters");
+    }
+
+    if(await getUserByUsername(username)) {
+        errors.push("Username already exists");
+    }
+    
+    if(errors.length > 0) {
+        return {errors};
+    }
+    
+    if(password !== confirmPassword) {
+        return {errors: ["Passwords do not match"]};
+    }
 
     const passwordHash = await bcrypt.hash(password, 10);
     await db.insert(users).values({name, username, passwordHash});
--- chapter2/blog-list/app/register/page.tsx
+++ chapter2/blog-list/app/register/page.tsx
@@ -1,22 +1,38 @@
+"use client"
+
 import {registerUser} from "@/app/actions/register";
+import {useActionState} from "react";
 
 const RegistrationPage = () => {
+    const [{errors}, formAction] = useActionState(registerUser, {errors: []})
+    
     const divStyle = {marginBottom: "1.5rem"}
     return (
         <div>
             <h2>Registration Page</h2>
-            <form action={registerUser}>
+            {errors?.length > 0 && (
+                <div style={{marginBottom: "1.5rem", padding: "0.5rem", border: "1px solid red"}}>
+                    {errors.map((error, index) => (
+                        <p key={index} style={{color: "red"}}>{error}</p>
+                    ))}
+                </div>
+            )}
+            <form action={formAction}>
                 <div style={divStyle}>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" />
+                    <input type="text" id="name" name="name" required />
                 </div>
                 <div style={divStyle}>
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" name="username" />
+                    <input type="text" id="username" name="username" required />
                 </div>
                 <div style={divStyle}>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" />
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <div style={divStyle}>
+                    <label htmlFor="confirmPassword">Confirm Password:</label>
+                    <input type="password" id="confirmPassword" name="confirmPassword" required />
                 </div>
                 <div>
                     <button type="submit">Register</button>
