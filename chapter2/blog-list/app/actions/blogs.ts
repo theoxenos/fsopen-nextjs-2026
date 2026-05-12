2e97c1e89fb7da94eb99ba249cd93b3981f7f8cf
Theoxenos <theoxenos@pm.me>
2026-05-12 11:20:13 +0200

    Finish exercise 4.14

--- chapter2/blog-list/app/actions/blogs.ts
+++ chapter2/blog-list/app/actions/blogs.ts
@@ -5,7 +5,12 @@ import {addBlog, updateBlogLikes} from "../services/blogService";
 import {revalidatePath} from "next/cache";
 import {auth} from "@/auth";
 
-export const createBlog = async (prevState: {error: string}, formData: FormData) => {
+export const createBlog = async (
+    prevState: {
+        errors: string[], 
+        values: {title?: string, url?: string, author?: string}
+    }, formData: FormData) => {
+    
     const session = await auth();
     if(!session) {
         redirect("/login");
@@ -15,14 +20,20 @@ export const createBlog = async (prevState: {error: string}, formData: FormData)
     const url = formData.get("url") as string;
     const author = formData.get("author") as string;
     
+    const errors = [];
+    
     if(!title || title.length < 5) {
-        return {error: "Title must be at least 5 characters long"};
+       errors.push("Title must be at least 5 characters long");
     }
     if(!url || url.length < 5) {
-        return {error: "Url must be at least 5 characters long"};
+        errors.push("Url must be at least 5 characters long");
     }
     if(!author || author.length < 5) {
-        return {error: "Author must be at least 5 characters long"};
+        errors.push("Author must be at least 5 characters long");
+    }
+
+    if(errors.length > 0) {
+        return {errors, values: {title, url, author}};
     }
 
     await addBlog({title, url, author});
--- chapter2/blog-list/app/blogs/new/page.tsx
+++ chapter2/blog-list/app/blogs/new/page.tsx
@@ -4,28 +4,31 @@ import {createBlog} from "@/app/actions/blogs";
 import {useActionState} from "react";
 
 const NewBlog = () => {
-    const [state, formAction] = useActionState(createBlog, {error: ""})
+    const initialValues = {title: "", author: "", url: ""};
+    const [state, formAction] = useActionState(createBlog, {errors: [], values: initialValues})
 
     return (
         <div>
             <h1>New Blog</h1>
-            {state.error && (
+            {state.errors?.length > 0 && (
                 <div style={{marginBottom: "1.5rem", padding: "0.5rem", border: "1px solid red"}}>
-                    <p style={{color: "red"}}>{state.error}</p>
+                    {state.errors.map((error, index) => (
+                        <p key={index} style={{color: "red"}}>{error}</p>
+                    ))}
                 </div>
             )}
             <form action={formAction}>
                 <div style={{marginBottom: "1.5rem"}}>
                     <label htmlFor="title">Title: </label>
-                    <input type="text" id="title" name="title" required={true}/>
+                    <input type="text" id="title" name="title" required={true} defaultValue={state.values?.title}/>
                 </div>
                 <div style={{marginBottom: "1.5rem"}}>
                     <label htmlFor="author">Author: </label>
-                    <input type="text" id="author" name="author" required={true}/>
+                    <input type="text" id="author" name="author" required={true} defaultValue={state.values?.author}/>
                 </div>
                 <div style={{marginBottom: "1.5rem"}}>
                     <label htmlFor="url">Url: </label>
-                    <input type="text" id="url" name="url" required={true}/>
+                    <input type="text" id="url" name="url" required={true} defaultValue={state.values?.url}/>
                 </div>
                 <button type="submit">Create Blog</button>
             </form>
